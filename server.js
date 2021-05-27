f249f2796ea7c1b58a7b2893302380c112ef0e1e
ishika-1702 <84778707+ishika-1702@users.noreply.github.com>
2021-05-27 22:42:49 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
