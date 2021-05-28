51c4e59386bf9cce8fd76ba024263a81e904b4f6
Rahul Singh <rahulsingh21247@gmail.com>
2021-05-28 15:28:43 +0530

    added stages jobs ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Jobs from './Components/jobUI/Jobs';
+import Stages from './Components/stagesUi/Stages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hello ther</h2>
+      <Jobs/>
+      <Stages/>
     </div>
   );
 }
